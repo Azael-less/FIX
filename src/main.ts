9c87d8206d9d64115af9a2f27edc59e5fe8c7225
Raul Ortiz Garcia <91501245+Azael-less@users.noreply.github.com>
2025-03-16 13:42:38 -0500

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -11,7 +11,6 @@ async function bootstrap() {
   app.enableCors({
     origin: process.env.CORS_ORIGIN || '*', // Ajusta en producción
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
     allowedHeaders: ['Content-Type', 'Authorization', 'x-apollo-operation-name', 'apollo-require-preflight'],
   });
 
